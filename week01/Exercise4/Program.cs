a7af86493ffcf8d8e428f0f635f7f53fe796be4d
Marinda Keller <mkeller@byupatheway.edu>
2025-10-31 16:37:32 -0600

    upload ex 4 and 5

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>(); //create the list
+
+        int number = -1;
+        //create the variables, Stop when 0 entered
+        int sum = 0;
+        int count = 0;
+        float average = 0;
+        int max = 0;
+
+        while (number != 0) //dont forget the parenthesis
+        {
+            Console.Write("Please enter a number or 0 to stop: ");
+            //Ask the user for a series of numbers
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+            //number was already declared an int so 
+            // int.parse wont have a prob w/change str to int
+
+            if (number != 0) //need both while and if !=0 statements
+            {
+                numbers.Add(number); // append numbers to a list.
+            }
+
+        }
+
+        foreach (int n in numbers)
+        { //is there another way to make this work?
+        }
+        Console.WriteLine($"Numbers List [" + string.Join(", ", numbers) + "]");
+
+
+        //Compute the sum, 
+        foreach (int i in numbers)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"The sum of your list is {sum}");
+
+        //Compute the average 
+        count = numbers.Count;
+        Console.WriteLine($"There are {count} numbers in your list.");
+        average = (float)sum / count; //here, count could have been numbers.Count, and skip the extra variable
+        Console.WriteLine($"The average of the numbers in your list is {average}.");
+
+        //Find the maximum (largest number)
+        foreach (int i in numbers)
+        {
+            if (i >= max)
+            {
+                max = i;
+            }
+        }
+        Console.WriteLine($"The largest number on your list is {max}.");
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        Console.WriteLine($"Hello, {userName}.");
+        int userNumber = PromptUserNumber();
+        int numberSquared = SquaredNumber(userNumber);
+        Console.WriteLine($"{userName}, your number, {userNumber}, is the square root of {numberSquared}.");
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {//Asks for and returns the user's name (as a string)
+        Console.Write("What can I call you? ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {//Asks for and returns the user number (as an integer)
+        Console.Write("Choose a number, any number. ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+    static int SquaredNumber(int userNumber)
+    {//Accepts an integer as a parameter and returns that number squared (as an integer)
+        int numberSquared = (userNumber * userNumber);
+        return numberSquared;
+    }
+    //DisplayResult - Accepts the user's name and the squared number and displays them.
+
 }
